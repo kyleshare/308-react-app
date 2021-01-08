db27caedb2a9c5176affbe563b12eb80d004d9e9
Kyle <kshare@calpoly.edu>
2021-01-08 13:59:43 -0800

    pass state from MyApp to Table

--- src/Table.js
+++ src/Table.js
@@ -17,6 +17,9 @@ function TableBody (props) {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
   });
@@ -27,13 +30,13 @@ function TableBody (props) {
    );
 }
 
-function Table(props) { 
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+function Table(props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 export default Table;
