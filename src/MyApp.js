379d0a11e9eeac05303323e94f397e8926cea3f2
Kyle <kshare@calpoly.edu>
2021-01-15 16:48:57 -0800

    Implement ID's in frontend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table'
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
@@ -12,8 +13,54 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  async function fetchAll(){
+   try {
+      //makes get request through our API on the backend
+      const response = await axios.get('http://localhost:5000/users');
+      console.log(response.data.users_list)
+      return response.data.users_list;     
+   }
+   catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+   }
+}
+
+  //call fetchAll function, if we get list of users update state of characters
+  //called once when MyApp comp. first mounts, (after that, changes are made by adding/removing 
+  //chars from the state)
+  useEffect(() => {
+   fetchAll().then( result => {
+      if (result)
+         setCharacters(result);
+    });
+}, [] );
+
+  async function makePostCall(person){
+   try {
+      //generate random id of 5 for person
+      person.id = generateId()
+      console.log(person.id);
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+   }
+   catch (error) {
+      console.log(error);
+      return false;
+   }
+}
+  function generateId(){
+    return Math.random().toString(36).substr(2, 5);
+  }
+
+  //why does this coditional run?
+  function updateList(person) { 
+     makePostCall(person).then( result => {
+     if (result.status === 201) {
+        setCharacters([...characters, person] );
+     }
+     });
   }
 
 
